68a9a824e42740a88c494de0200c80a10904614c
Alexey <lucker1st@gmail.com>
2021-03-30 10:02:33 +0500

    Закончил писать код для 1 таски

--- 1_task.py
+++ 1_task.py
@@ -2,7 +2,7 @@ from random import randint
 
 from math import *
 def funcSort(x):
-    if x>90:
+    if x>89.9:
         return x
     else:
         return x+1000
@@ -35,20 +35,34 @@ dictionary_rad={}
 for i in range(n):
     dictionary_values.update({i:gen_random(-100,100)})
     sqrtt=round(hypot((dictionary_values[i][0]),(dictionary_values[i][1])),5)
-
     degree=round(degrees(atan2((dictionary_values[i][1]),(dictionary_values[i][0]))),5)
     dictionary_length.update({i:sqrtt})
 
     dictionary_rad.update({i:degree})
-    # if (dictionary_values[i][0])>0:
-spisok=[]
-for i in dictionary_rad.values():
-    spisok.append(i)
-
 print(dictionary_values)
-print(dictionary_length)
+sort_values=(sorted(dictionary_rad.values(), key=funcSort))
 
-print(dictionary_rad)
-print(sorted(spisok, key=funcSort))
+sort_dictionary={}
+for i in range(len(sort_values)):
 
+    for k in dictionary_rad.keys():
 
+        if dictionary_rad[k]==sort_values[i]:
+            sort_dictionary.update({k:sort_values[i]})
+for i in sort_dictionary.keys():
+    print(f"Точка №{i}, координаты:",dictionary_values[i])
+max=0
+min=dictionary_length[0]
+average=0
+for i in dictionary_length.keys():
+    average+=dictionary_length[i]
+    if dictionary_length[i]>max:
+        max=dictionary_length[i]
+        k=i
+    if dictionary_length[i]<min:
+        min=dictionary_length[i]
+        j=i
+average=average/n
+print(f"Максимальное расстояние точки # {k} от центра с координатами{dictionary_values[k]}, длина",  max)
+print(f"Минимальное расстояние точки # {j} от центра с координатами{dictionary_values[j]}, длина",  min)
+print(f"Среднее расстояние точек от центра",  average)
